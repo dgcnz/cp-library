2cc32ebe39f07ff29daf10997bd75404ccc7f1a7
Diego Canez <canezdiego@gmail.com>
2021-01-30 18:01:20 -0500

    added ac's modint and new dijkstra

--- cplib/graphs/dijkstra.cpp
+++ cplib/graphs/dijkstra.cpp
@@ -1,41 +1,106 @@
-#include <bits/stdc++.h>
-using namespace std;
-using ll  = long long;
-using ii  = pair<int, int>;
-using vii = vector<ii>;
-using lli = pair<ll, int>;
 template <typename T>
-using pqueue = priority_queue<T>;
-
-const int NMAX = 1e5 + 11;
-const ll  INF  = 1e16;
-
-int n, m;
-vii g[NMAX]; // node, cost
-
-vector<ll> dijkstra(int start)
+struct Dijkstra
 {
-    pqueue<lli>  q;
-    vector<ll>   distance(n + 1, INF);
-    vector<bool> processed(n + 1, false);
+    using Graph = vector<vector<pair<int, T>>>;
+    T INF       = numeric_limits<T>::max();
 
-    distance[start] = 0;
-    q.push({0, start});
-    while (!q.empty())
+    Graph       g;
+    int         src, m = 0;
+    vector<int> p;
+    vector<T>   d;
+
+    Dijkstra(int n) : g(n), p(n), d(n){};
+    void add_edge(int u, int v, T w = 1) { g[u].emplace_back(v, w), m++; }
+
+    void run_dense(int src = 0)
     {
-        int u = q.top().second;
-        q.pop();
-        if (processed[u])
-            continue;
-        processed[u] = true;
-        for (auto [v, w] : g[u])
+        clear(src);
+
+        int          n = g.size();
+        vector<bool> vis(n, false);
+        d[src] = 0;
+        for (int i = 0; i < n; i++)
         {
-            if (distance[u] + w < distance[v])
+            int u = -1;
+            for (int v = 0; v < n; v++)
+                if (not vis[v] and (u == -1 or d[v] < d[u]))
+                    u = v;
+
+            if (d[u] == INF)
+                break;
+
+            vis[u] = true;
+            for (auto [v, w] : g[u])
             {
-                distance[v] = distance[u] + w;
-                q.push({-distance[v], v}); // negative weight because pq is >
+                if (d[u] + w < d[v])
+                {
+                    d[v] = d[u] + w;
+                    p[v] = u;
+                }
             }
         }
     }
-    return distance;
-}
+
+    void run_sparse(int src = 0)
+    {
+        clear(src);
+
+        set<pair<T, int>> q;
+        d[src] = 0;
+        q.emplace(d[src], src);
+        while (!q.empty())
+        {
+            int u = q.begin()->second;
+            q.erase(q.begin());
+
+            for (auto [v, w] : g[u])
+            {
+                if (d[u] + w < d[v])
+                {
+                    q.erase({d[v], v});
+                    d[v] = d[u] + w;
+                    p[v] = u;
+                    q.emplace(d[v], v);
+                }
+            }
+        }
+    }
+
+    vector<vector<pair<int, T>>> shortest_path_DAG(void) const
+    {
+        int                          n = g.size();
+        vector<vector<pair<int, T>>> dag(n);
+        for (int v = 0; v < n; ++v)
+            if (auto u = p[v]; u != -1)
+                dag[u].emplace_back(v, d[v] - d[u]);
+        return dag;
+    }
+
+    vector<int> shortest_path(int u) const
+    {
+        if (d[u] == INF)
+            return {};
+        vector<int> path;
+
+        for (int v = u; v != src; v = p[v])
+            path.push_back(v);
+        path.push_back(src);
+
+        reverse(begin(path), end(path));
+        return path;
+    }
+    void clear(int src)
+    {
+        this->src = src;
+        fill(begin(p), end(p), -1);
+        fill(begin(d), end(d), INF);
+    }
+    void operator()(int src = 0)
+    {
+        long long n = g.size();
+        if (n * n + m < (n + m) * log2(n))
+            run_dense(src);
+        else
+            run_sparse(src);
+    }
+};
--- cplib/math/combinatorics.hpp
+++ cplib/math/combinatorics.hpp
@@ -1,33 +1,31 @@
 #ifndef CPLIB_COMBINATORICS_HPP
 #define CPLIB_COMBINATORICS_HPP
 
+#include <atcoder/modint>
 #include <cassert>
-#include <cplib/math/modint.hpp>
 #include <vector>
 
-template <int NMAX, int MOD>
+template <int NMAX, typename mint>
 struct Combinations
 {
-    using mint = ModInt<MOD>;
     std::vector<mint> fact, inv_fact;
 
     Combinations(void) { precompute(NMAX + 1); }
 
     void precompute(int len)
     {
-        mint::precompute_inverses(len);
         fact.resize(len), inv_fact.resize(len);
         inv_fact[0] = fact[0] = 1;
         for (int i = 1; i < len; i++)
         {
-            inv_fact[i] = inv_fact[i - 1] * mint::inverse(i);
+            inv_fact[i] = inv_fact[i - 1] * mint(i).inv();
             fact[i]     = fact[i - 1] * i;
         }
     }
 
     mint C(int n, int k) const
     {
-        assert(fact.size() > n);
+        assert(int(fact.size()) > n);
         if (k > n or k < 0)
             return 0;
         return fact[n] * inv_fact[k] * inv_fact[n - k];
@@ -35,15 +33,16 @@ struct Combinations
 
     mint factorial(int n)
     {
-        assert(fact.size() > n);
+        assert(int(fact.size()) > n);
         return fact[n];
     }
 
     mint inverse_factorial(int n)
     {
-        assert(inv_fact.size() > n);
+        assert(int(fact.size()) > n);
         return inv_fact[n];
     }
+    mint operator()(int n, int k) const { return C(n, k); }
 };
 
 #endif // CPLIB_COMBINATORICS_HPP
